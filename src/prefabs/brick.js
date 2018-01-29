261d30d3a7e44c09c58bc0e2f024e1228b8ede0e
Casamandine <amandine.charles@ynov.com>
2018-01-29 10:36:17 +0100

    git

--- src/prefabs/brick.js
+++ src/prefabs/brick.js
@@ -1,11 +1,13 @@
+var color = "brick_0";
 // Documentation for Phaser's (2.6.2) sprites:: phaser.io/docs/2.6.2/Phaser.Sprite.html
 class Brick extends Phaser.Sprite {
 
   // initialization code in the constructor
   constructor(game, x, y, frame) {
-    super(game, x, y, 'brick_0', frame);
+    super(game, x, y, color, frame);
 
     this.anchor.setTo(0.5, 0.5);
+    this.vie = 2;
 
     if (this.game.physics.arcade){
       this.game.physics.arcade.enableBody(this);
